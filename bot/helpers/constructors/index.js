5278a916e8516ff3dc65fa4129373b56deabd315
GabrielMousquer0 <gabrielmousquer1227@gmail.com>
2025-05-14 13:00:06 -0300

    :art: Alterando estrutura de package para bot e helpers

---
+++ bot/helpers/constructors/index.js
@@ -0,0 +1,11 @@
+const constructToEmbed = require('./embed');
+const constructToButton = require('./button');
+const constructToActionRow = require('./row');
+const constructToSelectMenu = require('./selectMenu');
+
+module.exports = {
+  constructToActionRow,
+  constructToButton,
+  constructToEmbed,
+  constructToSelectMenu,
+};
--- bot/helpers/index.js
+++ bot/helpers/index.js
@@ -1,8 +1,10 @@
-const constructToEmbed = require('./construcEmbed');
-const constructToButton = require('./construtcButton');
-const constructToActionRow = require('./constructRow');
+const {
+  constructToActionRow,
+  constructToButton,
+  constructToEmbed,
+  constructToSelectMenu,
+} = require('./constructors');
 const messageDelete = require('./messageDelete');
-const constructToSelectMenu = require('./constructSelectMenu');
 const formatDuration = require('./formatDate');
 const removeDuplicates = require('./removeDuplicates');
 const removeItemFromArray = require('./filterArray');
